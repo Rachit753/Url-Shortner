2c2593f5cf3eb40b08417db2056de4ef04dd4cad
Rachit753 <chauhanrachit02@gmail.com>
2026-04-10 18:58:50 +0530

    fix: updated files for redis

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -1,15 +1,22 @@
 const Redis = require("ioredis");
 
+if (!process.env.REDIS_URL) {
+  throw new Error("REDIS_URL is NOT defined");
+}
+
+console.log("REDIS_URL =", process.env.REDIS_URL);
+
 const redis = new Redis(process.env.REDIS_URL, {
-    tls: {} 
+  tls: {},
+  maxRetriesPerRequest: null,
 });
 
 redis.on("connect", () => {
-    console.log("Redis Connected");
+  console.log("Redis Connected");
 });
 
 redis.on("error", (err) => {
-    console.error("Redis Error:", err.message);
+  console.error("Redis Error:", err.message);
 });
 
 module.exports = redis;
--- backend/src/queues/clickQueue.js
+++ backend/src/queues/clickQueue.js
@@ -1,10 +1,12 @@
 const Queue = require("bull");
 const redis = require("../config/redis");
 
-const clickQueue = new Queue("click-queue", {
+const clickQueue = new Queue("clickQueue", {
   redis: {
     host: redis.options.host,
     port: redis.options.port,
+    password: redis.options.password,
+    tls: {},
   },
 });
 
